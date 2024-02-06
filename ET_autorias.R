fca4a9589b2130eced0b4590c72fdf64a661c5de
Cesar A. Galvao <cesaraugusto.galvao@gmail.com>
2024-02-06 14:57:15 -0300

    autorias

--- ET_autorias.R
+++ ET_autorias.R
@@ -24,22 +24,50 @@ map_vec(function(x){
 
    # monta a lista de autorias
    autorias_list <- list()
-   errors <-c()
+   errors <-data.frame(
+     iteration = integer(),
+     code = character()
+   )
+   
+   #conf <- integer()
    
    for (i in 1:length(paths)) {
+     #conf <<- c(conf, i)
      tryCatch({
-       autorias_list[i] <- pega_autoria(paths[i])
+       autorias_list[[i]] <- pega_autoria(paths[i])
      }, error = function(e) {
-       errors <<- c(errors, i)
+       errors[i,] <<- c(i,codigos[i])
        # Code to handle errors
-       cat("Error in iteration", i, ":", conditionMessage(e), "\n")
      })
-   }
+   } #lista total tem 971, numero de parlamentares com código
+   errors <- errors %>% filter(!is.na(iteration))
+   #remove casos com length == 0, reduzimos para 555
+   autorias_list <- autorias_list[map_vec(autorias_list, length) != 0]
    
-   # elimina length == 0
-   autorias_list_filtered <- autorias_list[!(sapply(autorias_list, is.null))]
+   # erros retornam apenas codigos sem retorno da API
+   paths_errors <- errors$code |>
+     sort() |>
+     map_vec(function(x){
+       glue::glue("https://legis.senado.leg.br/dadosabertos/senador/{x}/autorias")
+     })
    
-   # trata os erros
+   #pega as que deram erro
+   autorias_errors <- map(paths_errors, pega_autoria)
+   #remove os null
+   autorias_errors <- autorias_errors[map_vec(autorias_errors, length) != 0]
    
+   #salva como tabela
+   saveRDS(bind_rows(autorias_list, autorias_errors), "tabelas/autorias.rds")
    
-} else {legislaturas <- readRDS("tabelas/autorias.rds")}
+ } else {autorias <- readRDS("tabelas/autorias.rds")}
+
+
+glimpse(autorias)
+
+autorias_sexo <- parlamentares %>%
+  select(codigo, sexo, partido) %>%
+  inner_join(autorias, by = c("codigo" = "cod_parlamentar")) %>%
+  rename("cod_materia" = Codigo)
+
+openxlsx::write.xlsx(autorias_sexo, "tabelas formatos usuário/autorias_sexo.xlsx", asTable = TRUE)
+openxlsx::write.xlsx(parlamentares, "tabelas formatos usuário/parlamentares.xlsx", asTable = TRUE)
